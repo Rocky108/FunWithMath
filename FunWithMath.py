68a6639dda68285f6ca0871fa83ade088dfa94fe
Rocky108 <dougald2001@gmail.com>
2019-10-14 08:53:17 -0500

    maths

--- FunWithMath.py
+++ FunWithMath.py
@@ -1,1 +1,27 @@
 # Starting off
+print(22/7)
+print(355/113)
+import math
+
+print(9801 / (2206*math.sqrt(2)))
+
+def archimedes(numSides):
+    innerAngleB=360.0/numSides
+    halfAngleA=innerAngleB/2
+    oneHalfSideS=math.sin(math.radians(halfAngleA))
+    sideS=oneHalfSideS*2
+    polygonCircumference=numSides*sideS
+    pi=polygonCircumference/2
+    return pi
+
+print(archimedes(8))
+print(archimedes(16))
+
+for sides in range(8, 100, 8):
+    print(sides, archimedes(sides))
+
+
+
+#experiment with loop above along with the actual value of pi. how many
+#sides does it take to make the two close?
+
