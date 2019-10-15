2ed54b346d985f7518449dcb6556e72b56872a8e
Rocky108 <dougald2001@gmail.com>
2019-10-15 09:04:00 -0500

    maths

--- FunWithMath.py
+++ FunWithMath.py
@@ -24,4 +24,29 @@ for sides in range(8, 100, 8):
 
 #experiment with loop above along with the actual value of pi. how many
 #sides does it take to make the two close?
+print(archimedes(30))
+# acumulators
+
+acc = 0
+for x in range(1, 6):
+    acc = acc+x
+
+print(acc)
+
+#compute the sum of the first 100 even numbers
+
+#compute the sum of the first 50 odd numbers
+
+#compute the average of the first 100 odd numbers
+
+#write a function that returns the average of the first n numbers, where n is a parameter
+
+#write a function called factoral that computes the product of the first n numbers, where n is a parameter
+# each number in the fibonacci sequence is the sum of the previous two numbers
+# the first two numbers in the sequence ar 1 and 1. compute the 10th
+# fibonacci number.
+#20
+#write a function to compute the nth fibonacci number, where n is a parameter
+
+# you may assume n will be greater than or equal to 3.
 
